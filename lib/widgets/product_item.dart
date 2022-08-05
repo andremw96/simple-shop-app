9ebd05d227d48360536ecd436210db60b5ba20a7
andremw96 <andre19961@gmail.com>
2022-08-05 12:11:40 +0700

    first commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem(
+      {Key? key, required this.id, required this.title, required this.imageUrl})
+      : super(key: key);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      footer: GridTileBar(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.favorite),
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_cart),
+        ),
+      ),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
