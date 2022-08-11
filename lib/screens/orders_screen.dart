ac144f36a22a5b11b7af7fa90f924423a10145ca
andremw96 <andre19961@gmail.com>
2022-08-11 15:58:48 +0700

    add app drawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,13 +5,18 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/providers/orders.dart' show Order;
 import 'package:shop_app/widgets/order_item.dart';
 
+import '../widgets/app_drawer.dart';
+
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
+
+  static const routeName = "/order";
 
   @override
   Widget build(BuildContext context) {
     final orders = Provider.of<Order>(context);
     return Scaffold(
+      drawer: const AppDrawer(),
       appBar: AppBar(
         title: Text("Your Orders"),
       ),
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/products_provider.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/badge.dart';
 import '../providers/cart.dart';
 import '../widgets/product_grid.dart';
@@ -24,6 +25,7 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const AppDrawer(),
       appBar: AppBar(
         title: const Text("My Shop"),
         actions: [
