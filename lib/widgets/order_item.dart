8f849ab67786579041335033da78b3c154ac99d0
andremw96 <andre19961@gmail.com>
2022-09-16 14:19:12 +0700

    animation order item

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -20,55 +20,66 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("\$${widget.orderItem.amount}"),
-            subtitle: Text(
-              DateFormat(" dd MMM yyyy hh:mm")
-                  .format(widget.orderItem.datetime),
-            ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.orderItem.products.length * 20.0 + 10, 180),
-              child: ListView.builder(
-                itemCount: widget.orderItem.products.length,
-                itemBuilder: (context, i) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        widget.orderItem.products[i].title,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        "${widget.orderItem.products[i].quantity}x \$${widget.orderItem.products[i].price}",
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  );
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded
+          ? min(widget.orderItem.products.length * 20.0 + 10, 180)
+          : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("\$${widget.orderItem.amount}"),
+              subtitle: Text(
+                DateFormat(" dd MMM yyyy hh:mm")
+                    .format(widget.orderItem.datetime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
                 },
               ),
+            ),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                height: _expanded
+                    ? min(widget.orderItem.products.length * 20.0 + 10, 180)
+                    : 0,
+                child: ListView.builder(
+                  itemCount: widget.orderItem.products.length,
+                  itemBuilder: (context, i) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.orderItem.products[i].title,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "${widget.orderItem.products[i].quantity}x \$${widget.orderItem.products[i].price}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
